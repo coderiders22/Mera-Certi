5949086c3f96145bad92c5207c33c1823a948c26
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2023-11-06 02:12:39 +0530

    audio fixed

--- New_certi/app/src/main/java/com/example/new_certi/page9_proceed_EWS_certi.java
+++ New_certi/app/src/main/java/com/example/new_certi/page9_proceed_EWS_certi.java
@@ -3,8 +3,10 @@ package com.example.new_certi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.widget.Button;
@@ -12,6 +14,12 @@ import android.widget.Button;
 public class page9_proceed_EWS_certi extends AppCompatActivity {
 
     private Button button;
+    Button gallery;
+    Button gallery2;
+    Button gallery3;
+    Button gallery4;
+    Button gallery5;
+
 
 
 
@@ -19,6 +27,53 @@ public class page9_proceed_EWS_certi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page9_proceed_ews_certi);
+
+        gallery=findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+        gallery2=findViewById(R.id.gallery2);
+        gallery2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+        gallery3=findViewById(R.id.gallery3);
+        gallery3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+        gallery4=findViewById(R.id.gallery4);
+        gallery4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+        gallery5=findViewById(R.id.gallery5);
+        gallery5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+
         button = findViewById(R.id.button17_6);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +82,14 @@ public class page9_proceed_EWS_certi extends AppCompatActivity {
             }
         });
 
+
+
+
+
     }
+
+
+
     public void openActivity2(){
         Intent intent = new Intent(this, page10.class);
         startActivity(intent);
