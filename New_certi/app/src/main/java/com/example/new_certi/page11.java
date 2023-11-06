6de68300fbaa892649e527bfa5a0c9d586b77c53
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2023-11-06 20:48:21 +0530

    update

--- New_certi/app/src/main/java/com/example/new_certi/page11.java
+++ New_certi/app/src/main/java/com/example/new_certi/page11.java
@@ -1,29 +1,56 @@
-package com.example.new_certi;
+
+        package com.example.new_certi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.new_certi.R;
+import com.example.new_certi.page12;
 
 public class page11 extends AppCompatActivity {
 
     private Button button;
+    Button gallery;
+    EditText userName;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page11);
         button = findViewById(R.id.button19);
+
+        gallery=findViewById(R.id.gallery7);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 openActivity2();
+
+
+
             }
         });
+
 
     }
     public void openActivity2(){
